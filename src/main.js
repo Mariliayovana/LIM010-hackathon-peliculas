843f44fd70c945e8c0bb25f4980f3f3fcbf0999e
Rtaperuvian <ritagomezh@gmail.com>
2019-08-01 15:23:56 -0500

    Terminado de funcionabilidad primaria

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
-  const showData = (data) => {
-    let allData = '';
-  data.forEach(objeto => { 
-    return allData += `
-      <img class="" src=${objeto.Poster} alt="${objeto.Title}">
-      <p class="">${objeto.Title}</p>
-      <p class="">Sinopsis: ${objeto.Plot}</p>
-      <p class="">Actores: ${objeto.Actors}</p>
-      <p class="">Tiempo de duración: ${objeto.Runtime}</p>
-      <p class="">Director: ${objeto.Director}</p>
-      <p class="">Escritor: ${objeto.Writer}</p>
-      <p class="">País: ${objeto.Country}</p>
-      <p class="">Año: ${objeto.Year}</p>
-      <p class=""> Género: ${objeto.Genre}</p>`
-  });
-    document.getElementById('probando').innerHTML += allData;
-};
+showData = (objeto) => {
+  return  `
+    <img class="rounded mx-auto d-block" src=${objeto.Poster} alt="${objeto.Title}">
+    <div class ="text-center">
+    <p class="">${objeto.Title}</p>
+    <p class="">Sinopsis: ${objeto.Plot}</p>
+    <p class="">Actores: ${objeto.Actors}</p>
+    <p class="">Tiempo de duración: ${objeto.Runtime}</p>
+    <p class="">Director: ${objeto.Director}</p>
+    <p class="">Escritor: ${objeto.Writer}</p>
+    <p class="">País: ${objeto.Country}</p>
+    <p class="">Año: ${objeto.Year}</p>
+    <p class=""> Género: ${objeto.Genre}</p>
+    </div>`
+
+
+}
 
 document.getElementById('potterImage').addEventListener('click',()=>{
 for(let i=0; i< hp.length;i++){
