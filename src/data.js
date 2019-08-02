33f9254d3f09a03a126553a32abd700e5ef446b4
gabhu-dev <gabhu@hotmail.es>
2019-08-02 02:46:28 -0500

    subiendo arreglos en primera estructura

--- src/data.js
+++ src/data.js
@@ -1,51 +1,47 @@
-
-
-const getData = (titleId) =>{  
-  let data = [];
-  fetch(`http://www.omdbapi.com/?i=tt${titleId}&apikey=13dc3330`)
-  .then(response => response.json())
-  .then(output => {
-    data.push(output);
-    showData(data);
-    
-  })
+const getData = (titleId) => {
+    let data = [];
+    fetch(`http://www.omdbapi.com/?i=tt${titleId}&apikey=13dc3330`)
+        .then(response => response.json())
+        .then(output => {
+            data.push(output);
+            showData(data);
+            filter(data);
+            console.log(data);
+            console.log(output);
+        })
 };
 
 
 
-document.getElementById('anilloImage').addEventListener('click',() =>{
-  showMovies()
+document.getElementById('anilloImage').addEventListener('click', () => {
+    showMovies()
 })
 
-const hp =['0241527','0295297','0304141','0330373','0373889','0417741','0926084','1201607'];
-function showMovies (){
-  hp.map (function (datos) {
-  fetch(`http://www.omdbapi.com/?i=tt`+ datos + `&apikey=13dc3330`)
- .then(response => response.json())
-  .then(output => {
-    document.getElementById('probando').innerHTML += showData(output);
-  })
-})
+const hp = ['0241527', '0295297', '0304141', '0330373', '0373889', '0417741', '0926084', '1201607'];
+
+function showMovies() {
+    hp.map(function(datos) {
+        fetch(`http://www.omdbapi.com/?i=tt` + datos + `&apikey=13dc3330`)
+            .then(response => response.json())
+            .then(output => {
+                document.getElementById('probando').innerHTML += showData(output);
+            })
+    })
 }
 
-document.getElementById('potterImage').addEventListener('click',() =>{
-  showMoviesS()
+document.getElementById('potterImage').addEventListener('click', () => {
+    showMoviesS()
 })
-const lotr = ['0120737','0167261','0167260','0903624','1170358','2310332'];
-function showMoviesS (){
-  lotr.map (function (datos) {
-  fetch(`http://www.omdbapi.com/?i=tt`+ datos + `&apikey=13dc3330`)
- .then(response => response.json())
-  .then(output => {
+const lotr = ['0120737', '0167261', '0167260', '0903624', '1170358', '2310332'];
 
-  document.getElementById('probando').innerHTML += showData(output);
-  })
+function showMoviesS() {
+    lotr.map(function(datos) {
+        fetch(`http://www.omdbapi.com/?i=tt` + datos + `&apikey=13dc3330`)
+            .then(response => response.json())
+            .then(output => {
 
-})
+                document.getElementById('probando').innerHTML += showData(output);
+            })
+
+    })
 }
-
-  
-
-
-
-  
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 const showData = (objeto) => {
-    return  `<div class="card">
+    return `<div class="card">
                         <img class="card-img-top img-fluid" src=${objeto.Poster} alt="${objeto.Title}">
                         <p class="card-title-center">${objeto.Title}</p>
                       </div>`
-  }
- 
+}
+
 // showData = (objeto) => {
 //   return  `
 //     <img class="rounded mx-auto d-block" src=${objeto.Poster} alt="${objeto.Title}">
@@ -21,30 +21,55 @@ const showData = (objeto) => {
 //     </div>`
 // }
 
-document.getElementById('potterImage').addEventListener('click',()=>{
-for(let i=0; i< hp.length;i++){
-  getData(hp[i]);
+//buscador
+const form = document.querySelector('#form');
+const button = document.querySelector('#button');
+const result = document.querySelector('#probando');
+const filter = (data) => {
+    result.innerHTML = '';
+    console.log(form.value);
+    const text = form.value.toLowerCase();
+    for (let pelicula of data) {
+        let title = pelicula.Title.toLowerCase();
+        if (title.indexOf(text) !== -1) {
+            result.innerHTML += `
+         <li>${pelicula.Title} - Image:${pelicula.Post}</li>
+        `
+        }
+    }
+    if (result.innerHTML === '') {
+        result.innerHTML += `
+<li>pelicula no encontrada...</li>`
+    }
 }
-document.getElementById('carousel').classList.toggle('ocultar');
-document.getElementById('potterImage').classList.toggle('ocultar');
-document.getElementById('anilloImage').classList.toggle('ocultar');
+button.addEventListener('click', filter)
+form.addEventListener('keyup', filter)
+filter();
+
+
+document.getElementById('potterImage').addEventListener('click', () => {
+    for (let i = 0; i < hp.length; i++) {
+        getData(hp[i]);
+    }
+    document.getElementById('carousel').classList.toggle('ocultar');
+    document.getElementById('potterImage').classList.toggle('ocultar');
+    document.getElementById('anilloImage').classList.toggle('ocultar');
 });
 
-document.getElementById('anilloImage').addEventListener('click',()=>{
-  for(let i=0; i< lotr.length;i++){
-    getData(lotr[i]);
-  }
-  document.getElementById('carousel').classList.toggle('ocultar');
-  document.getElementById('potterImage').classList.toggle('ocultar');
-  document.getElementById('anilloImage').classList.toggle('ocultar');
+document.getElementById('anilloImage').addEventListener('click', () => {
+    for (let i = 0; i < lotr.length; i++) {
+        getData(lotr[i]);
+    }
+    document.getElementById('carousel').classList.toggle('ocultar');
+    document.getElementById('potterImage').classList.toggle('ocultar');
+    document.getElementById('anilloImage').classList.toggle('ocultar');
 
 });
 
-document.getElementById('pelis').addEventListener('click', () =>{
-  document.getElementById('potterImage').classList.toggle('ocultar');
-  document.getElementById('anilloImage').classList.toggle('ocultar');
-  document.getElementById('probando').classList.toggle('mostrar');
+document.getElementById('pelis').addEventListener('click', () => {
+    document.getElementById('potterImage').classList.toggle('ocultar');
+    document.getElementById('anilloImage').classList.toggle('ocultar');
+    document.getElementById('probando').classList.toggle('mostrar');
 
-  
-}
-)
+
+})
