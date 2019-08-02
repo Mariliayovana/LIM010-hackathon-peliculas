62bc522eb1edc0794ae4e4c5615d0d832869c41c
Rtaperuvian <ritagomezh@gmail.com>
2019-08-02 11:36:57 -0500

    terminando

--- README.md
+++ README.md
@@ -1,27 +1,31 @@
 # SAGAZ
-
 ## Preámbulo
-
 Con la aparición de plataformas para ver películas en la nube, como Netflix o
 Hulu, ver películas desde donde estés, comiendo lo que quieras
 (sin tener que pagar precios inadecuados) ha cambiado la experiencia del usuario
 y modificado el mercado de las películas a nivel global.
-
 ## Resumen del proyecto
-
-Porque sabemos que el estado emocional es un factor importante, decidimos crear una plataforma que sólo muestre una selección de películas de la saga de Harry Potter  &  El señor de los Anillos . Con esto queremos que nuestro público tenga un lugar al cual pueda recordar los momentos mas interesantes que te pueda llevar la magia, la lucha y vencer las adversidades.Y lo puedas compartir  en familia o con amigos.
-
-## Consideraciones generales
-
+Porque sabemos que el estado emocional es un factor importante, decidimos crear una plataforma que sólo muestre una selección de películas de la saga de Harry Potter  &  El señor de los Anillos. Con esto queremos que nuestro público tenga un lugar al cual pueda recordar los momentos mas interesantes que te pueda llevar la magia, la lucha y vencer las adversidades.Y lo puedas compartir  en familia o con amigos.
+## Research
 Nuestro público objetivo principal son personas de 18 a 35 años. Que se encuentran trabajando; son personas que se mantienen ocupadas y en su tiempo libre deciden ver las SAGAS, para que pueda distraerse de la presión de cada día, ya que su estado de ánimo es un factor importante para decidir qué quieren ver.
+## Testeo de usabilidad
+Tuvimos uno prototipo de baja fidelidad para testear a nuestros usuarios y poder recibir feedback, ya que nuestro objetivo es que la página sea muy agradable y accesible para ellos.
+<img src ="./src/img/1.png">
+<img src ="./src/img/2.png">
+<img src ="./src/img/3.png">
 
+## Feedback
+* Modificamos el boton de buscar de lugar.
+* Cambiamos mobile por el flip car.
+* Cambiamos en nombre de RAPI SEARCH por SAGAZ.
+## Final del proyecto
+- Primera vista :
+<img src="./src/img/image.png">
+
+- Segunda vista :
+<img src="./src/img/image (1).png">
 ## Integrantes:
-
 * Rita Gómez Huisa - (gitHub(Rtaperuvian)).
 * Sara Abigail Sulca Zuloaga -(gitHub(sarAbigail)).
 * Gabriela Ignacio Roque - (git(Gabhu-dev)).
 * Marilia Condor Rojas - (git(mariliayovana)).
-
-
-
-
--- src/data.js
+++ src/data.js
@@ -1,46 +1,51 @@
 
-const getData = (titleId) => {
+
+const getData = (titleId) =>{  
     let data = [];
-    
     fetch(`http://www.omdbapi.com/?i=tt${titleId}&apikey=13dc3330`)
-        .then(response => response.json())
-        .then(output => {
-            data.push(output);
-            showData(data);
-         })
+    .then(response => response.json())
+    .then(output => {
+      data.push(output);
+      showData(data);
       
-};
-
-
-const hp = ['0241527', '0295297', '0304141', '0330373', '0373889', '0417741', '0926084', '1201607'];
-
-
-showMovies = () => {
-    hp.map(getDates = (datos) => {
-        fetch(`http://www.omdbapi.com/?i=tt` + datos + `&apikey=13dc3330`)
-            .then(response => response.json())
-            .then(output => {
-                document.getElementById('probando').innerHTML += showData(output);
-            })
     })
-}
-
-document.getElementById('potterImage').addEventListener('click', () => {
+  };
+  
+  
+  
+  document.getElementById('anilloImage').addEventListener('click',() =>{
+    showMovies()
+  })
+  
+  const hp =['0241527','0295297','0304141','0330373','0373889','0417741','0926084','1201607','3183660','4123430'];
+  function showMovies (){
+    hp.map (function (datos) {
+    fetch(`http://www.omdbapi.com/?i=tt`+ datos + `&apikey=13dc3330`)
+   .then(response => response.json())
+    .then(output => {
+      document.getElementById('probando').innerHTML += showData(output);
+    })
+  })
+  }
+  
+  document.getElementById('potterImage').addEventListener('click',() =>{
     showMoviesS()
-})
-const lotr = ['0120737', '0167261', '0167260', '0903624', '1170358', '2310332'];
-
-showMoviesS = () => {
-    lotr.map(getDates = (datos) => {
-        fetch(`http://www.omdbapi.com/?i=tt` + datos + `&apikey=13dc3330`)
-            .then(response => response.json())
-            .then(output => {
-
-                document.getElementById('probando').innerHTML += showData(output);
-            })
-
+  })
+  const lotr = ['0120737','0167261','0167260','0903624','1170358','2310332'];
+  function showMoviesS (){
+    lotr.map (function (datos) {
+    fetch(`http://www.omdbapi.com/?i=tt`+ datos + `&apikey=13dc3330`)
+   .then(response => response.json())
+    .then(output => {
+  
+    document.getElementById('probando').innerHTML += showData(output);
     })
-
-})
-
-}
+  
+  })
+  }
+  
+    
+  
+  
+  
+    
